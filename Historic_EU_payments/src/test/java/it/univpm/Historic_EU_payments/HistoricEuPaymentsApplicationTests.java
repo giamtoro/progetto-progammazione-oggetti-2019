07ad913b7bb665e44daa55f9ea415e7a3c98c434
Gianmarco troni <gianmarcotroni97@outlook.it>
2019-11-20 16:51:33 +0100

    base

---
+++ Historic_EU_payments/src/test/java/it/univpm/Historic_EU_payments/HistoricEuPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.Historic_EU_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricEuPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
