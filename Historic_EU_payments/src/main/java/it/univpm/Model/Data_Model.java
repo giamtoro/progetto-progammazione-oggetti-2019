97f90c9f818991b859054735c0fd054fafe87513
Gianmarco troni <gianmarcotroni97@outlook.it>
2019-11-21 22:27:20 +0100

    inserimeto nel modello datti atributo mancante

--- Historic_EU_payments/src/main/java/it/univpm/Model/Data_Model.java
+++ Historic_EU_payments/src/main/java/it/univpm/Model/Data_Model.java
@@ -7,6 +7,7 @@ public class Data_Model {
     private String Country ="";
     private String NUTS1_ID ="";
     private String NUTS2_ID ="";
+    private String NUTS2_name ="";
     private String Fund ="";
     private int Year =0;
     private int[] Programming_Period = {0,0};
@@ -17,34 +18,36 @@ public class Data_Model {
     
     public Data_Model() {
 		super();
-		Country = "";
-		NUTS1_ID = "";
-		NUTS2_ID = "";
-		Fund = "";
-		Year = 0;
+		this.Country = "";
+		this.NUTS1_ID = "";
+		this.NUTS2_ID = "";
+		this.NUTS2_name ="";
+		this.Fund = "";
+		this.Year = 0;
 		int[] r = {0,0};
-		Programming_Period = r;
-		EU_Payment_annual = 0;
-		Modelled_annual_expenditure = 0;
-		Standard_Deviation_of_annual_expenditure = 0;
-		Standard_Error_of_modelled_annual_expenditure = 0;
+		this.Programming_Period = r;
+		this.EU_Payment_annual = 0;
+		this.Modelled_annual_expenditure = 0;
+		this.Standard_Deviation_of_annual_expenditure = 0;
+		this.Standard_Error_of_modelled_annual_expenditure = 0;
 	}
     
     
-	public Data_Model(String country, String nUTS1_ID, String nUTS2_ID, String fund, int year, int[] programming_Period,
+	public Data_Model(String country, String nUTS1_ID, String nUTS2_ID,String nUTS2_name, String fund, int year, int[] programming_Period,
 			int eU_Payment_annual, int modelled_annual_expenditure, int standard_Deviation_of_annual_expenditure,
 			int standard_Error_of_modelled_annual_expenditure) {
 		super();
-		Country = country;
-		NUTS1_ID = nUTS1_ID;
-		NUTS2_ID = nUTS2_ID;
-		Fund = fund;
-		Year = year;
-		Programming_Period = programming_Period;
-		EU_Payment_annual = eU_Payment_annual;
-		Modelled_annual_expenditure = modelled_annual_expenditure;
-		Standard_Deviation_of_annual_expenditure = standard_Deviation_of_annual_expenditure;
-		Standard_Error_of_modelled_annual_expenditure = standard_Error_of_modelled_annual_expenditure;
+		this.Country = country;
+		this.NUTS1_ID = nUTS1_ID;
+		this.NUTS2_ID = nUTS2_ID;
+		this.NUTS2_name = nUTS2_name;
+		this.Fund = fund;
+		this.Year = year;
+		this.Programming_Period = programming_Period;
+		this.EU_Payment_annual = eU_Payment_annual;
+		this.Modelled_annual_expenditure = modelled_annual_expenditure;
+		this.Standard_Deviation_of_annual_expenditure = standard_Deviation_of_annual_expenditure;
+		this.Standard_Error_of_modelled_annual_expenditure = standard_Error_of_modelled_annual_expenditure;
 	}
 
 
@@ -75,6 +78,16 @@ public class Data_Model {
 
 	protected void setNUTS2_ID(String nUTS2_ID) {
 		NUTS2_ID = nUTS2_ID;
+	}
+
+    
+	protected String getNUTS2_name() {
+		return NUTS2_name;
+	}
+
+
+	protected void setNUTS2_name(String nUTS2_name) {
+		NUTS2_name = nUTS2_name;
 	}
 
 
@@ -151,7 +164,7 @@ public class Data_Model {
 	@Override
 	public String toString() {
 		return "Data_Model [Country=" + getCountry() + ", NUTS1_ID=" + getNUTS1_ID() + ", NUTS2_ID="
-				+ getNUTS2_ID() + ", Fund=" + getFund() + ", Year=" + getYear() + ", Programming_Period="
+				+ getNUTS2_ID() +", NUTS_name="+ getNUTS2_name() +", Fund=" + getFund() + ", Year=" + getYear() + ", Programming_Period="
 				+ Arrays.toString(getProgramming_Period()) + ", EU_Payment_annual=" + getEU_Payment_annual()
 				+ ", Modelled_annual_expenditure=" + getModelled_annual_expenditure()
 				+ ", Standard_Deviation_of_annual_expenditure=" + getStandard_Deviation_of_annual_expenditure()
